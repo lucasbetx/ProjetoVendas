c271113b3907aea2c5db3330cce3b0fcff574869
Lucas Teixeira <lucasussx@outlook.com>
2020-09-14 14:41:35 -0300

    14/09

--- WebSales/Program.cs
+++ WebSales/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebSales
+namespace WebSales.Models.ViewModels
 {
     public class Program
     {
