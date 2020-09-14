7db10f1ae2af410ed03a9bb7c59a4031549529e8
Lucas Teixeira <lucasussx@outlook.com>
2020-09-14 16:49:12 -0300

    16:49

--- WebSales/Startup.cs
+++ WebSales/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebSales.Data;
+using WebSales.Services;
 
 namespace WebSales
 {
@@ -41,6 +42,7 @@ namespace WebSales
                         builder.MigrationsAssembly("WebSales")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
